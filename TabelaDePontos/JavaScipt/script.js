f8982c0baf5955e08875a848f37d19e9a044eda1
Jonatan Trindade <109476031+Jonata-tr@users.noreply.github.com>
2022-09-23 19:46:01 -0400

    Resolvido problemas de undefined
    Resolvido o problema que resultava nos pontos se tornarem valores undefined ao adicionar vitorias ou empates. Com isso a tabela esta completa e totalmente funcional.

--- TabelaDePontos/JavaScipt/script.js
+++ TabelaDePontos/JavaScipt/script.js
@@ -1,16 +1,17 @@
 
-var jogador1 = {nome:"", vitorias: 0, derrotas: 0, empates:0, pontos:0 }
-var jogador2 = {nome:"", vitorias: 0, derrotas: 0, empates:0, pontos:0 }
-var jogador3 = {nome:"", vitorias: 0, derrotas: 0, empates:0, pontos:0 }
-var jogador4 = {nome:"", vitorias: 0, derrotas: 0, empates:0, pontos:0 }
+var jogador1 = {nome:"", vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
+var jogador2 = {nome:undefined, vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
+var jogador3 = {nome:undefined, vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
+var jogador4 = {nome:undefined, vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
 
 var listaJogadores = [jogador1, jogador2, jogador3, jogador4];
 
+console.log(jogador1.pontos)
+
 
 function calcularPontos(jogador) {
-  var classificação = jogador.vitorias * 3 + jogador.empates;
-  console.log(jogador.pontos)
-  jogador.pontos = classificação;
+  var classificação = (jogador.vitorias * 3) + jogador.empates;
+  return classificação
 }
 
 jogador1.pontos = calcularPontos(jogador1);
@@ -27,15 +28,18 @@ function adicionarJogador() {
     listaJogadores[indice].nome = jogador;
     imprimirJogadoresNaTela(listaJogadores);
     indice++;
+    if (indice == 4) {
+      document.getElementById("butao").hidden = true
+      document.getElementById("input").hidden = true
+    }
     return indice;
-  } else if(indice == listaJogadores.length) {
-    window.alert("Numero maximo de jogadores atingido.");
   }
 }
 
 function imprimirJogadoresNaTela(listaJogadores) {
   var tabela = "";
   for (var i = 0; i < listaJogadores.length; i++) {
+    if (listaJogadores[i].nome != undefined){
     tabela += "<tr><th>" + listaJogadores[i].nome + "</th>";
     tabela += "<td>" + listaJogadores[i].vitorias + "</td>";
     tabela += "<td>" + listaJogadores[i].derrotas + "</td>";
@@ -56,11 +60,26 @@ function imprimirJogadoresNaTela(listaJogadores) {
     }
     var table = document.getElementById("placarPontos");
     table.innerHTML = tabela;
+    document.getElementById("input").value = ""
   }
+}
 
 function adicionarVitoria(i) {
   var jogador= listaJogadores[i];
   jogador.vitorias++;
+  jogador.pontos = calcularPontos(jogador);
+  imprimirJogadoresNaTela(listaJogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador= listaJogadores[i];
+  jogador.derrotas++;
+  imprimirJogadoresNaTela(listaJogadores);
+}
+
+function adicionarEmpate(i) {
+  var jogador= listaJogadores[i];
+  jogador.empates++;
   jogador.pontos = calcularPontos(jogador);
   imprimirJogadoresNaTela(listaJogadores);
 }
